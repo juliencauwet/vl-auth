c312c63a85029652d5fd7139e4809056610af9d9
julien <juliencauwet@yahoo.fr>
2019-04-24 10:59:17 +0200

    first commit

---
+++ src/main/java/com/greenwin/VLauth/VlAuthApplication.java
@@ -0,0 +1,13 @@
+package com.greenwin.VLauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VlAuthApplication.class, args);
+	}
+
+}
